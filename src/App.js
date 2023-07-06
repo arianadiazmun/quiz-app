49aadc080dfa84c12e02f57efb3fe1a47da3de3e
Ariana Diaz <arianadiaz@Arianas-MacBook-Air.local>
2023-07-06 16:51:39 -0400

    to finish

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./styles/App.css";
-import Home from "./pages/Home";
+//import Home from "./pages/Home";
 import Err404 from "./pages/Err404";
 import Quiz from "./pages/Quiz";
 
@@ -10,7 +10,7 @@ function App() {
    <BrowserRouter>
    <Routes>
     <Route path="/quiz" element={<Quiz/>}/>
-  <Route path= "/home" element= {<Home/>}/>
+  <Route path= "/" element= {<Quiz/>}/>
   <Route path="*" element= {<Err404 />}/>
    </Routes>
    </BrowserRouter>
